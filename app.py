09e2e3877db90b23e1af6619cb705467b24dadd9
Sato <s_ychm@yahoo.co.jp>
2019-03-12 14:03:41 +0900

    ランダムなサイコロ結果表示を追加

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,6 +8,13 @@ app = Flask(__name__)
 @app.route('/', methods=['get'])
 def index():
     return render_template('index.html', message='Hi Hachimantai!')
+
+
+@app.route('/dice', methods=['GET'])
+def dice():
+    result = random.randint(1, 6)
+
+    return render_template('dice.html', result=result)
 
 
 if __name__ == '__main__':
